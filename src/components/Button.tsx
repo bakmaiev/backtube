765ab76b989a66fac12af25a412c424706ba69f0
bakmaiev <bakmaiev@gmail.com>
2024-01-03 23:43:27 +0200

    Сreate Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,41 @@
+import { VariantProps, cva } from "class-variance-authority";
+import { ComponentProps } from "react";
+import { twMerge } from "tailwind-merge";
+
+const buttonStyles = cva(["hover:bg-secondary-hover", "transition-colors"], {
+  variants: {
+    variant: {
+      default: ["bg-secondary", "hover:bg-secondary-hover"],
+      ghost: ["hover:bg-amber-100"],
+    },
+    size: {
+      default: [],
+      icon: [
+        "rounded-full",
+        "w-10",
+        "h-10",
+        "flex",
+        "justify-center",
+        "items-center",
+        "p-2.5",
+      ],
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+    size: "default",
+  },
+});
+
+type ButtonProps = VariantProps<typeof buttonStyles> & ComponentProps<"button">;
+
+const Button = ({ variant, size, className, ...props }: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={twMerge(buttonStyles({ variant, size }), className)}
+    />
+  );
+};
+
+export default Button;
